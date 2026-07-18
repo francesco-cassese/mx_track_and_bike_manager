a2a1aee80069226da8ef89fd6520a487374462dc
Francesco Cassese <francescocassese9@gmail.com>
2026-07-19 01:55:20 +0200

    fix: resolve unresolved db connection promise and missing import extension

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import connection from '../config/db';
+import connection from '../config/db.js';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
